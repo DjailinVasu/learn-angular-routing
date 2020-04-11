4ffa5ad078cd1058925c9d3e4e1aae430a8d63b6
Mary Denysova <djailin.vasu@gmail.com>
2020-04-11 20:09:51 +0300

    Защита дочерних роутов

--- src/app/auth.guard.ts
+++ src/app/auth.guard.ts
@@ -3,13 +3,15 @@ import {
   ActivatedRouteSnapshot,
   RouterStateSnapshot,
   Router,
+  CanActivateChild,
 } from "@angular/router";
 import { Observable } from "rxjs";
 import { Injectable } from '@angular/core';
 import { AuthService } from './auth.service';
+import { ThrowStmt } from '@angular/compiler';
 
 @Injectable({providedIn: 'root'})
-export class AuthGuard implements CanActivate {
+export class AuthGuard implements CanActivate, CanActivateChild {
 
   constructor(
     private authServise: AuthService,
@@ -34,4 +36,12 @@ export class AuthGuard implements CanActivate {
        }
      })
   }
+
+  canActivateChild(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): boolean | Observable<boolean> | Promise<boolean>  {
+      return this.canActivate(route, state)
+  }
+
 }
